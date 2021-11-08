c8bdea653fc790752c531e486135fb52c19aebac
HP <ngocbaopham1606@gmail.com>
2021-11-08 22:43:02 +0700

    Add project files.

---
+++ BaiTapNhom/FormTKHDon.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace BaiTapNhom
+{
+    partial class FormTKHDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboMaNCC = new System.Windows.Forms.ComboBox();
+            this.DataGrid_TimKiemNH = new System.Windows.Forms.DataGridView();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.txtNgaynhap = new System.Windows.Forms.TextBox();
+            this.cboMaNhap = new System.Windows.Forms.ComboBox();
+            this.radioNgaynhap = new System.Windows.Forms.RadioButton();
+            this.radioMaNCC = new System.Windows.Forms.RadioButton();
+            this.radioMaNhap = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid_TimKiemNH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboMaNCC
+            // 
+            this.cboMaNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboMaNCC.FormattingEnabled = true;
+            this.cboMaNCC.Location = new System.Drawing.Point(243, 150);
+            this.cboMaNCC.Name = "cboMaNCC";
+            this.cboMaNCC.Size = new System.Drawing.Size(363, 33);
+            this.cboMaNCC.TabIndex = 36;
+            // 
+            // DataGrid_TimKiemNH
+            // 
+            this.DataGrid_TimKiemNH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGrid_TimKiemNH.Location = new System.Drawing.Point(74, 261);
+            this.DataGrid_TimKiemNH.Name = "DataGrid_TimKiemNH";
+            this.DataGrid_TimKiemNH.RowHeadersWidth = 51;
+            this.DataGrid_TimKiemNH.RowTemplate.Height = 24;
+            this.DataGrid_TimKiemNH.Size = new System.Drawing.Size(644, 166);
+            this.DataGrid_TimKiemNH.TabIndex = 35;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(627, 178);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(134, 39);
+            this.btnThoat.TabIndex = 33;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.Location = new System.Drawing.Point(627, 118);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(134, 39);
+            this.btnTimKiem.TabIndex = 34;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // txtNgaynhap
+            // 
+            this.txtNgaynhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNgaynhap.Location = new System.Drawing.Point(243, 199);
+            this.txtNgaynhap.Name = "txtNgaynhap";
+            this.txtNgaynhap.Size = new System.Drawing.Size(363, 30);
+            this.txtNgaynhap.TabIndex = 32;
+            // 
+            // cboMaNhap
+            // 
+            this.cboMaNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboMaNhap.FormattingEnabled = true;
+            this.cboMaNhap.Location = new System.Drawing.Point(243, 99);
+            this.cboMaNhap.Name = "cboMaNhap";
+            this.cboMaNhap.Size = new System.Drawing.Size(363, 33);
+            this.cboMaNhap.TabIndex = 31;
+            // 
+            // radioNgaynhap
+            // 
+            this.radioNgaynhap.AutoSize = true;
+            this.radioNgaynhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioNgaynhap.Location = new System.Drawing.Point(40, 199);
+            this.radioNgaynhap.Name = "radioNgaynhap";
+            this.radioNgaynhap.Size = new System.Drawing.Size(131, 29);
+            this.radioNgaynhap.TabIndex = 28;
+            this.radioNgaynhap.TabStop = true;
+            this.radioNgaynhap.Text = "Ngày Nhập";
+            this.radioNgaynhap.UseVisualStyleBackColor = true;
+            // 
+            // radioMaNCC
+            // 
+            this.radioMaNCC.AutoSize = true;
+            this.radioMaNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioMaNCC.Location = new System.Drawing.Point(40, 150);
+            this.radioMaNCC.Name = "radioMaNCC";
+            this.radioMaNCC.Size = new System.Drawing.Size(197, 29);
+            this.radioMaNCC.TabIndex = 29;
+            this.radioMaNCC.TabStop = true;
+            this.radioMaNCC.Text = "Mã Nhà Cung Cấp";
+            this.radioMaNCC.UseVisualStyleBackColor = true;
+            // 
+            // radioMaNhap
+            // 
+            this.radioMaNhap.AutoSize = true;
+            this.radioMaNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioMaNhap.Location = new System.Drawing.Point(40, 99);
+            this.radioMaNhap.Name = "radioMaNhap";
+            this.radioMaNhap.Size = new System.Drawing.Size(113, 29);
+            this.radioMaNhap.TabIndex = 30;
+            this.radioMaNhap.TabStop = true;
+            this.radioMaNhap.Text = "Mã Nhập";
+            this.radioMaNhap.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(389, 44);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "TÌM KIẾM HÓA ĐƠN";
+            // 
+            // FormTKHDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboMaNCC);
+            this.Controls.Add(this.DataGrid_TimKiemNH);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtNgaynhap);
+            this.Controls.Add(this.cboMaNhap);
+            this.Controls.Add(this.radioNgaynhap);
+            this.Controls.Add(this.radioMaNCC);
+            this.Controls.Add(this.radioMaNhap);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTKHDon";
+            this.Text = "TÌM KIẾM HÓA ĐƠN";
+            this.Load += new System.EventHandler(this.FormTKHDon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid_TimKiemNH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboMaNCC;
+        private System.Windows.Forms.DataGridView DataGrid_TimKiemNH;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.TextBox txtNgaynhap;
+        private System.Windows.Forms.ComboBox cboMaNhap;
+        private System.Windows.Forms.RadioButton radioNgaynhap;
+        private System.Windows.Forms.RadioButton radioMaNCC;
+        private System.Windows.Forms.RadioButton radioMaNhap;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BaiTapNhom/FormTKHDon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapNhom
+{
+    public partial class FormTKHDon : Form
+    {
+        public FormTKHDon()
+        {
+            InitializeComponent();
+        }
+
+        private void FormTKHDon_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
